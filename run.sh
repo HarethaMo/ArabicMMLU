9b4039a764dc0cf0fff1ac3cfe6b77a094c9d0e0
HarethaMo <haretha07@gmail.com>
2024-05-05 20:56:45 +0300

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,47 +1,19 @@
-## Jais models
+## Cohere models
 
-python evaluate.py --load_8bit --base_model='core42/jais-13b' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-13b' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='core42/jais-13b' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-13b' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/aya-101' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/aya-101' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
 
-python evaluate.py --load_8bit --base_model='core42/jais-13b-chat' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-13b-chat' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='core42/jais-13b-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-13b-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-plus' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-plus' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
 
-python evaluate.py --load_8bit --base_model='core42/jais-30b-v3' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-v3' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-v3' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-v3' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
-
-python evaluate.py --load_8bit --base_model='core42/jais-30b-chat-v3' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-chat-v3' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-chat-v3' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='core42/jais-30b-chat-v3' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-v01' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-v01' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
 
 
-## AceGPT models
+## Llama3 models
 
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
 
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B-chat' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B-chat' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-7B-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
-
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B' --lora_weights='x' --lang_alpa='ar' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
-
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B-chat' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
-python evaluate.py --load_8bit --base_model='FreedomIntelligence/AceGPT-13B-chat' --lora_weights='x' --lang_alpa='en' --lang_prompt='ar'
-
-
-
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar'
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en'
