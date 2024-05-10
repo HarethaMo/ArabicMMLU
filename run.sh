f56059d2e38b186a4ec146a37cb0c91066b2f9f3
HarethaMo <haretha07@gmail.com>
2024-05-10 16:27:31 +0300

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 ## Cohere models
 
-python evaluate.py --load_8bit --base_model='CohereForAI/aya-101' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/aya-101'
+#python evaluate.py --load_8bit --base_model='CohereForAI/aya-101' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/aya-101'
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/Llama-3-70B'
+python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en' --cache_dir='/ibex/user/abush0a/Llama-3-70B'
 python evaluate.py --load_8bit --base_model='CohereForAI/aya-101' --lora_weights='x' --lang_alpa='en' --lang_prompt='en' --cache_dir='/ibex/user/abush0a/aya-101'
 
 python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-plus' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/c4ai-command-r-plus'
@@ -12,8 +14,5 @@ python evaluate.py --load_8bit --base_model='CohereForAI/c4ai-command-r-v01' --l
 
 ## Llama3 models
 
-python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/Llama-3-70B'
-python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-70B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en' --cache_dir='/ibex/user/abush0a/Llama-3-70B'
-
-python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/Llama-3-8B-Instruct'
-python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en' --cache_dir='/ibex/user/abush0a/Llama-3-8B-Instruct'
+# python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='ar' --lang_prompt='ar' --cache_dir='/ibex/user/abush0a/Llama-3-8B-Instruct'
+# python evaluate.py --load_8bit --base_model='meta-llama/Meta-Llama-3-8B-Instruct' --lora_weights='x' --lang_alpa='en' --lang_prompt='en' --cache_dir='/ibex/user/abush0a/Llama-3-8B-Instruct'
